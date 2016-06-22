3a614bce04b057ddcc010894f21ffabf035d52ee
blackman1380 <mousavi.asadollah@gmail.com>
2016-06-22 21:41:34 +0430

    Update xamarin.lua

--- bot/xamarin.lua
+++ bot/xamarin.lua
@@ -269,7 +269,7 @@ function create_config( )
    "weather",
    "wiki",
     },
-    sudo_users = {231026530,222002038},--Sudo users
+    sudo_users = {228282039},--Sudo users
     moderation = {data = 'data/moderation.json'},
     about_text = [[ ]],
     help_text_realm = [[ ]],
