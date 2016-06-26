27ba810ee078f3741bbfea405b1fbf8b62284ae7
telegrambot81 <telegrambot81@gmail.com>
2016-06-26 16:58:10 +0430

    Update xamarin.lua

--- bot/xamarin.lua
+++ bot/xamarin.lua
@@ -269,7 +269,7 @@ function create_config( )
    "weather",
    "wiki",
     },
-    sudo_users = {228282039},--Sudo users
+    sudo_users = {120629055,106789762},--Sudo users
     moderation = {data = 'data/moderation.json'},
     about_text = [[ ]],
     help_text_realm = [[ ]],
