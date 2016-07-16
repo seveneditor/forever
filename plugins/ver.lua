2bf4236b5c0ebf156f6eb305af3bc3fa6d9f4678
telegrambot81 <telegrambot81@gmail.com>
2016-07-16 17:19:44 +0430

    Add files via upload

---
+++ plugins/ver.lua
@@ -0,0 +1,27 @@
+do
+
+function run(msg, matches)
+  return [[ K I N G B O T
+-----------------------------------
+A new bot for manage your Supergroups.
+-----------------------------------
+@Xx_HACKER_KHOFFASH_xX #Developer
+-----------------------------------
+@SECRET_HACK3R #Manager
+-----------------------------------
+Bot number : +989137727210
+-----------------------------------
+Bot version : 2.7 ]]
+https://telegram.me/KINGBOT_TG
+end
+
+return {
+  description = "Shows bot version", 
+  usage = "version: Shows bot version",
+  patterns = {
+    "^[#!/]version$"
+  }, 
+  run = run 
+}
+
+end
